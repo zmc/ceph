63889910be32109f84c45a9225cccc429b2b0d9f
Zack Cerza <zack@redhat.com>
2018-10-10 15:29:02 -0400

    implement version service
    needs tests

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+
+import * as _ from 'lodash';
+import { BehaviorSubject, Subscription } from 'rxjs';
+
+import { ExecutingTask } from '../models/executing-task';
+import { ServicesModule } from './services.module';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: ServicesModule
 })
 export class VersionService {
+  // Observable sources
+  private versionDataSource = new BehaviorSubject(null);
 
-  constructor() { }
+  // Observable streams
+  versionData$ = this.versionDataSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router, private ngZone: NgZone) {
+    this.refresh();
+  }
+  refresh() {
+    this.http.get('api/public/version').subscribe((data) => {
+      this.versionDataSource.next(data);
+    });
+
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        this.ngZone.run(() => {
+          this.refresh();
+        });
+      }, 5000);
+    });
+  }
+
 }
